8554edc1e121e3d1ea870e5619450616348c272e
Yobe Zhou <33565334+YobeZhou@users.noreply.github.com>
2021-12-22 11:09:03 +0800

    Show Windows console when app is running (#28)
    Co-authored-by: zhouyuebiao <837574599@qq.com>

--- main.c
+++ main.c
@@ -50,6 +50,9 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR szCmdLi
     /*Initialize the HAL for LittlevGL*/
     lv_win32_init(hInstance, SW_SHOWNORMAL, 800, 480, NULL);
 
+    /*Output prompt information to the console, you can also use printf() to print directly*/
+    LV_LOG_USER("LVGL initialization completed!");
+
     /*Run the demo*/
     lv_demo_widgets();
 
